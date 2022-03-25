362160d927103577e110ef0a4b4db28c18f84834
Rynco Maekawa <lynzrand@outlook.com>
2022-03-25 19:58:31 +0800

    feat: skip already scanned files

--- pack.py
+++ pack.py
@@ -43,24 +43,24 @@ class PackState():
 
         console.log("Loading database")
         self.db = sqlite3.connect(path)
-        self.db.execute("pragma journal_mode = WAL")
-        self.db.execute("""
-            create table if not exists
-            pkg (
-                hash blob,
-                path text,
-                ty int,
-                ix_time real
-            )
-        """)
-        self.db.execute("""
-            create index if not exists
-            ix_pkg_path
-            on pkg (
-                path asc
-            )
-        """)
-        self.db.commit()
+        with self.db:
+            self.db.execute("pragma journal_mode = WAL")
+            self.db.execute("""
+                create table if not exists
+                pkg (
+                    path text,
+                    hash blob,
+                    ty int,
+                    ix_time real
+                )
+            """)
+            self.db.execute("""
+                create index if not exists
+                ix_pkg_hash
+                on pkg (
+                    hash asc
+                )
+            """)
 
     _disp: live.Live
     _node_scan_prog: progress.Progress
@@ -79,6 +79,8 @@ class PackState():
             self._build_display()
             self._disp.start()
             for p in path.rglob("*"):
+                rel_path = p.relative_to(rel).as_posix()
+
                 self._inode_cnt += 1
                 self._node_scan_prog.update(self._tid_node_scan,
                                             total=self._inode_cnt)
@@ -102,10 +104,11 @@ class PackState():
             progress.TextColumn(
                 "{task.description}",
                 justify="right",
-                table_column=table.Column(width=30),
+                table_column=table.Column(ratio=2),
             ),
-            progress.BarColumn(None),
-            progress.MofNCompleteColumn(),
+            progress.BarColumn(None, table_column=table.Column(ratio=5)),
+            progress.MofNCompleteColumn(table_column=table.Column(ratio=3)),
+            expand=True,
         )
 
         self._file_size_prog = progress.Progress(
@@ -113,27 +116,34 @@ class PackState():
                 "{task.description}",
                 markup=False,
                 justify="right",
-                table_column=table.Column(width=30),
+                table_column=table.Column(ratio=2),
             ),
-            progress.BarColumn(None),
-            progress.DownloadColumn(True),
-            progress.TimeElapsedColumn(),
-            progress.TimeRemainingColumn(),
+            progress.BarColumn(None, table_column=table.Column(ratio=5)),
+            progress.DownloadColumn(True, table_column=table.Column(ratio=1)),
+            progress.TimeElapsedColumn(table_column=table.Column(ratio=1)),
+            progress.TimeRemainingColumn(table_column=table.Column(ratio=1)),
+            expand=True,
         )
 
         tbl.add_row(self._node_scan_prog)
         tbl.add_row(self._file_size_prog)
-        self._disp = live.Live(tbl,
-                               transient=True,
-                               console=console,
-                               refresh_per_second=10)
+        self._disp = live.Live(
+            tbl,
+            transient=True,
+            console=console,
+            refresh_per_second=5,
+        )
 
-        self._tid_node_scan = self._node_scan_prog.add_task("Files scanned",
-                                                            start=False,
-                                                            total=0)
-        self._tid_whole_scan = self._file_size_prog.add_task("Data scanned",
-                                                             start=False,
-                                                             total=0)
+        self._tid_node_scan = self._node_scan_prog.add_task(
+            "Files scanned",
+            start=False,
+            total=0,
+        )
+        self._tid_whole_scan = self._file_size_prog.add_task(
+            "Data scanned",
+            start=False,
+            total=0,
+        )
         return self._disp
 
     def _add_path(self, rel: Path, path: Path):
@@ -148,7 +158,30 @@ class PackState():
         curr_file = None
         try:
             rel_path = path.relative_to(rel).as_posix()
+            db_data = next(
+                self.db.execute(
+                    "select * from pkg where path = ?",
+                    (rel_path, ),
+                ),
+                None,
+            )
+
             stat = path.stat()
+
+            rescan = True
+            if db_data is not None:
+                last_index_time = db_data[3]
+                if db_data[1] is not None and stat.st_mtime <= last_index_time:
+                    rescan = False
+                    console.log(f"Skipping already scanned file {path}")
+
+            if not rescan:
+                # Skip the file and mark it as scanned
+                self._file_size_prog.advance(self._tid_whole_scan,
+                                             stat.st_size)
+                self._node_scan_prog.advance(self._tid_node_scan)
+                return
+
             curr_file = self._file_size_prog.add_task(
                 rel_path,
                 total=stat.st_size,
@@ -162,30 +195,80 @@ class PackState():
                     self._file_size_prog.advance(curr_file, rd)
                     self._file_size_prog.advance(self._tid_whole_scan, rd)
             with self.db:
-                self.db.execute(
-                    "insert into pkg values(:hash, :path, :ty, :ix_time)", {
-                        "hash": hasher.digest(),
-                        "path": rel_path,
-                        "ty": TY_FILE,
-                        "ix_time": time()
-                    })
+                if db_data:
+                    self.db.execute(
+                        """
+                        update pkg set hash = :hash, ty = :ty, ix_time = :ix_time
+                        where path = :path
+                        """, {
+                            "hash": hasher.digest(),
+                            "path": rel_path,
+                            "ty": TY_FILE,
+                            "ix_time": time()
+                        })
+                else:
+                    self.db.execute(
+                        """
+                        insert into pkg values(:path, :hash, :ty, :ix_time)
+                        """, {
+                            "path": rel_path,
+                            "hash": hasher.digest(),
+                            "ty": TY_FILE,
+                            "ix_time": time()
+                        })
             self._node_scan_prog.advance(self._tid_node_scan)
         finally:
             if curr_file is not None:
                 self._file_size_prog.remove_task(curr_file)
 
     def _add_dir(self, rel: Path, path: Path):
+        rel_path = path.relative_to(rel).as_posix()
+        db_data = next(
+            self.db.execute(
+                "select * from pkg where path = ?",
+                (rel_path, ),
+            ),
+            None,
+        )
+        stat = path.stat()
+        full_rescan = True
+        if db_data is not None:
+            last_index_time = db_data[3]
+            if stat.st_mtime < last_index_time:
+                full_rescan = False
+                console.log(f"Skipping already scanned folder {path}")
+
         for v in path.iterdir():
-            self._add_path(rel, v)
+            if v.is_dir() or full_rescan:
+                self._add_path(rel, v)
+            else:
+                self._file_size_prog.advance(self._tid_whole_scan,
+                                             v.stat().st_size)
+                self._node_scan_prog.advance(self._tid_node_scan)
+
         # insert after all contents are indexed
         with self.db:
-            self.db.execute(
-                "insert into pkg values(:hash, :path, :ty, :ix_time)", {
-                    "hash": None,
-                    "path": path.relative_to(rel).as_posix(),
-                    "ty": TY_DIR,
-                    "ix_time": time()
-                })
+            if db_data:
+                self.db.execute(
+                    """
+                        update pkg set hash = :hash, ty = :ty, ix_time = :ix_time
+                        where path = :path
+                        """, {
+                        "hash": None,
+                        "path": rel_path,
+                        "ty": TY_DIR,
+                        "ix_time": time()
+                    })
+            else:
+                self.db.execute(
+                    """
+                    insert into pkg values(:path, :hash, :ty, :ix_time)
+                    """, {
+                        "hash": None,
+                        "path": rel_path,
+                        "ty": TY_DIR,
+                        "ix_time": time()
+                    })
         self._node_scan_prog.advance(self._tid_node_scan)
 
     def flush(self):
@@ -214,10 +297,28 @@ def main():
     ap = argparse.ArgumentParser("yuunagi-pack")
     makeParser(ap)
     ns = ap.parse_args()
-    ps = PackState(ns.database)
-    for d in ns.source:
-        ps.add_path(Path(ns.relative_to).absolute(), Path(d).absolute())
-    ps.close()
+    try:
+        ps = PackState(ns.database)
+        for d in ns.source:
+            ps.add_path(Path(ns.relative_to).absolute(), Path(d).absolute())
+
+        result_by_type = {
+            x[1]: x[0]
+            for x in ps.db.execute(
+                "select count(*), ty from pkg group by ty order by ty asc")
+        }
+        console.log("Scan completed.")
+        file_cnt = result_by_type[TY_FILE] if TY_FILE in result_by_type else 0
+        dir_cnt = result_by_type[TY_DIR] if TY_DIR in result_by_type else 0
+        console.log(
+            f"Total: [bold]{file_cnt}[/bold] files and [bold]{dir_cnt}[/bold] directories."
+        )
+    except KeyboardInterrupt:
+        console.log(
+            "Progress interrupted. Restart with the same arguments to resume.")
+        pass
+    finally:
+        ps.close()
 
 
 if __name__ == "__main__": main()
